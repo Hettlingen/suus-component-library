f0a94cd7b98fa42200299c9c58faef02b45f85dc
Hettlingen <martinbraun@scoop.ch>
2026-05-06 19:20:52 +0200

    adjust package.json with valid component library name

--- src/index.ts
+++ src/index.ts
@@ -2,3 +2,5 @@
 
 // export { default as InputButton } from "./components/button/InputButton";
 // export type { InputButtonProps } from "./components/button/InputButton";
+
+export { default as InputButton } from "./components/form/buttons/input-button/input-button";
--- vite.config.ts
+++ vite.config.ts
@@ -1,7 +1,35 @@
-import { defineConfig } from 'vite'
-import react from '@vitejs/plugin-react'
+import { defineConfig } from "vite";
+import react from "@vitejs/plugin-react";
+import dts from "vite-plugin-dts";
+import { resolve } from "node:path";
 
-// https://vite.dev/config/
+/**
+ * "external: ["react", "react-dom", "react/jsx-runtime"]
+ * Damit wird React nicht in dein Package gebundled. Das ist wichtig, damit die Nutzer deiner Library nicht mehrere React-Instanzen haben, was zu Problemen führen kann.
+ */
 export default defineConfig({
-  plugins: [react()],
-})
+  plugins: [
+    react(),
+    dts({
+      insertTypesEntry: true,
+    }),
+  ],
+  build: {
+    lib: {
+      entry: resolve(__dirname, "src/index.ts"),
+      name: "SUUS Component Library",
+      fileName: "suus-component-library",
+      formats: ["es", "umd"],
+    },
+    rollupOptions: {
+      external: ["react", "react-dom", "react/jsx-runtime"],
+      output: {
+        globals: {
+          react: "React",
+          "react-dom": "ReactDOM",
+          "react/jsx-runtime": "jsxRuntime",
+        },
+      },
+    },
+  },
+});
