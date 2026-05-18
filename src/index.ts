fd042c575b385791f1353c16f8a742b942811a31
Hettlingen <martinbraun@scoop.ch>
2026-05-18 16:56:10 +0200

    add components to the index file

--- src/index.ts
+++ src/index.ts
@@ -1,7 +1,39 @@
 // Import global styles
 import "./styles/index.css";
 
-
 // Export all components here
 export { default as Button } from "./components/form/button/Button";
 export type { ButtonProps, ColorToken } from "./components/form/button/Button";
+
+export { default as Checkbox } from "./components/form/checkbox/Checkbox";
+export type { CheckboxProps } from "./components/form/checkbox/Checkbox";
+
+export { default as Chip } from "./components/form/chip/Chip";
+export type { ChipProps } from "./components/form/chip/Chip";
+
+export { default as DatePicker } from "./components/form/date-picker/DatePicker";
+export type { DatePickerProps } from "./components/form/date-picker/DatePicker";
+
+export { default as Dropdown } from "./components/form/dropdown/Dropdown";
+export type { DropdownProps } from "./components/form/dropdown/Dropdown";
+
+export { default as Link } from "./components/form/link/Link";
+export type { LinkProps } from "./components/form/link/Link";
+
+export { default as Password } from "./components/form/password/Password";
+export type { PasswordProps } from "./components/form/password/Password";
+
+export { default as PostalCode} from "./components/form/postal-code/PostalCode";
+export type { PostalCodeProps, PostalCodeLocality, PostalCodeSuggestion } from "./components/form/postal-code/PostalCode";
+
+export { default as RadioButton} from "./components/form/radiobutton/RadioButton";
+export type { RadioButtonProps, RadioButtonOption } from "./components/form/radiobutton/RadioButton";
+
+export { default as Switch} from "./components/form/switch/Switch";
+export type { SwitchProps } from "./components/form/switch/Switch";
+
+export { default as TextField} from "./components/form/textfield/TextField";
+export type { TextFieldProps } from "./components/form/textfield/TextField";
+
+export { default as Upload} from "./components/form/upload/Upload";
+export type { UploadProps } from "./components/form/upload/Upload";
